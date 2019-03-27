1da295b2085c6825a1ee95d70423cac4eadf5fe8
visualstudio <visualstudio@122-PC37>
2019-03-27 12:30:12 -0400

    call back

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,30 +8,60 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Player
-    {
-        public bool IsAlive
-        {
-            get { return health > 0; }
-        }
-        int health = 100;
-        public void Hit()
-        {
-            Random r = new Random();
-            health -= r.Next(5, 50);
-        }
-    }
     class Program
-    {
-        static void Main(string[] args)
-        {
-            Player player = new Player();
-            for (int i = 0; i < 20; i++)
-            {
-                player.Hit();
-                Console.WriteLine("Is player alive: " + player.IsAlive);
 
-            }
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("downloading file");
+
+            Download();
+
+            Console.ReadLine();
+
+
+
         }
+
+
+
+        static void Download()
+
+        {
+
+            Network.Download((message) => Console.WriteLine("Download complete " + message));
+
+        }
+
     }
+
+
+
+    // Imaginary external network library
+
+
+
+    class Network
+
+    {
+
+        static public void Download(Action callback)
+
+        {
+
+            Task.Run(() => {
+
+                Thread.Sleep(3000);
+
+                callback("completed");
+
+            });
+
+        }
+
+    }
+
 }
